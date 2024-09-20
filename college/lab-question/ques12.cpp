c68e70be397fb5a2a5ebe2e5895dcbf62817d056
Achyut-shekhar <achyutshekhar54@gmail.com>
2024-09-21 00:07:24 +0530

    completed code

--- college/lab-question/ques12.cpp
+++ college/lab-question/ques12.cpp
@@ -20,8 +20,40 @@ public:
     this->minutes = minutes;
     this->seconds = seconds;
   }
+  void normalize()
+  {
+    if (seconds >= 60)
+    {
+      minutes += seconds / 60;
+      seconds %= 60;
+    }
+    if (minutes >= 60)
+    {
+      hour += minutes / 60;
+      minutes %= 60;
+    }
+  }
+  Time addtime(Time &t1, Time &t2)
+  {
+    Time result;
+    result.hour = t1.hour + t2.hour;
+    result.minutes = t1.minutes + t2.minutes;
+    result.seconds = t1.seconds + t2.seconds;
+    result.normalize();
+    return result;
+  }
+  void display()
+  {
+    cout << "hours: " << hour << "\tminutes: " << minutes << "\tseconds: " << seconds << endl;
+  }
 };
 int main()
 {
   Time time1(1, 20, 35);
+  Time time2(1, 45, 50);
+
+  Time time3;
+  time3 = time3.addtime(time1, time2);
+
+  time3.display();
 }
