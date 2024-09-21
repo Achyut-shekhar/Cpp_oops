6df045ad63d4db71765d7b4506113d0e81ee790c
Achyut-shekhar <achyutshekhar54@gmail.com>
2024-09-21 11:03:19 +0530

    added one more question

---
+++ college/lab-question/ques13.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class savingAccount
+{
+  double savingBalance;
+  double monthlyInterest;
+
+public:
+  static double annualInterestRate;
+
+  savingAccount(int saver)
+  {
+    savingBalance = saver;
+    monthlyInterest = 0.0;
+  }
+  void monthlyinterest()
+  {
+    monthlyInterest = (annualInterestRate * savingBalance) / 12;
+    savingBalance += monthlyInterest;
+  }
+  static void modifyInterest(double rate)
+  {
+
+    annualInterestRate = rate;
+  }
+
+  void display()
+  {
+    cout << "balance is " << savingBalance << endl;
+    cout << "monthly interest is Rs" << monthlyInterest << endl;
+  }
+};
+double savingAccount::annualInterestRate = 0.04;
+
+int main()
+{
+  savingAccount saver1(2000);
+  savingAccount saver2(3000);
+
+  saver1.monthlyinterest();
+  saver2.monthlyinterest();
+
+  cout << "\n-----Saver1------" << endl;
+  saver1.display();
+  cout << "\n-----Saver2------" << endl;
+  saver2.display();
+
+  cout << "\n------interest rate is 5-----" << endl;
+  savingAccount::modifyInterest(0.05);
+
+  saver1.monthlyinterest();
+  cout << "\n-----Saver1------" << endl;
+  saver1.display();
+
+  saver2.monthlyinterest();
+  cout << "\n-----Saver1------" << endl;
+  saver2.display();
+}
